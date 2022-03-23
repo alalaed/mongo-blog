9c24d8a866407a743a93e3405f3d6655d6a34682
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-03-23 16:24:20 +0200

    routes are implemented

--- src/services/blogs/index.js
+++ src/services/blogs/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import createError from "http-errors";
 import blogsModel from "./model.js";
-
+import q2m from "query-to-mongo";
 const blogsRouter = express.Router();
 
 blogsRouter.post("/", async (req, res, next) => {
@@ -15,13 +15,33 @@ blogsRouter.post("/", async (req, res, next) => {
   }
 });
 
+// blogsRouter.get("/", async (req, res, next) => {
+//   try {
+//     const allBlogs = await blogsModel.find();
+//     res.send(allBlogs);
+//   } catch (error) {
+//     next(error);
+//     console.log(error);
+//   }
+// });
+
 blogsRouter.get("/", async (req, res, next) => {
   try {
-    const allBlogs = await blogsModel.find();
-    res.send(allBlogs);
+    const mongoQuery = q2m(req.query);
+    const total = await blogsModel.countDocuments(mongoQuery.criteria);
+    const blogs = await blogsModel
+      .find(mongoQuery.criteria, mongoQuery.options.fields)
+      .limit(mongoQuery.options.limit || 20)
+      .skip(mongoQuery.options.skip || 0)
+      .sort(mongoQuery.options.sort); // no matter in which order you call this methods, Mongo will ALWAYS do SORT, SKIP, LIMIT in this order
+    res.send({
+      links: mongoQuery.links(`http://localhost:3001/blogs`, total),
+      total,
+      totalPages: Math.ceil(total / mongoQuery.options.limit),
+      blogs,
+    });
   } catch (error) {
     next(error);
-    console.log(error);
   }
 });
 
@@ -108,6 +128,89 @@ blogsRouter.post("/:blogId/comments", async (req, res, next) => {
       next(
         createError(404, `Blog with id ${req.body.bookId} has no comments!`)
       );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.get("/:blogId/comments", async (req, res, next) => {
+  try {
+    const blog = await blogsModel.findById(req.params.blogId);
+    if (blog) {
+      res.send(blog.comments);
+    } else {
+      next(createError(404, `Blog with id ${req.params.blogId} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.get("/:blogId/comments/:commentId", async (req, res, next) => {
+  try {
+    const blog = await blogsModel.findById(req.params.blogId);
+    if (blog) {
+      const comment = blog.comments.find(
+        (comment) => comment._id.toString() === req.params.commentId
+      );
+
+      if (comment) {
+        res.send(comment);
+      } else {
+        next(
+          createError(404, `comment with id ${req.params.commentId} not found!`)
+        );
+      }
+    } else {
+      next(createError(404, `blog with id ${req.params.blogId} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.put("/:blogId/comments/:commentId", async (req, res, next) => {
+  try {
+    const blog = await blogsModel.findById(req.params.blogId);
+    if (blog) {
+      const index = blog.comments.findIndex(
+        (comment) => comment._id.toString() === req.params.commentId
+      );
+
+      if (index !== -1) {
+        blog.comments[index] = {
+          ...blog.comments[index].toObject(),
+          ...req.body,
+        };
+
+        await blog.save();
+        res.send(blog);
+      } else {
+        next(
+          createError(404, `comment with id ${req.params.commentId} not found!`)
+        );
+      }
+    } else {
+      next(createError(404, `blog with id ${req.params.blogId} not found!`));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.delete("/:blogId/comments/:commentId", async (req, res, next) => {
+  try {
+    const blogToModify = await blogsModel.findByIdAndUpdate(
+      req.params.blogId,
+      { $pull: { comments: { _id: req.params.commentId } } },
+      { new: true }
+    );
+
+    if (blogToModify) {
+      res.send(blogToModify);
+    } else {
+      next(createError(404, `Blog with id ${req.params.blogId} not found!`));
     }
   } catch (error) {
     next(error);
