bcde512797b895577787bbecc94b2922cccb84a7
Ali Alaeddine <94364910+alalaed@users.noreply.github.com>
2022-03-24 15:25:27 +0200

    authorsrouter implemented

--- src/server.js
+++ src/server.js
@@ -1,38 +1,43 @@
-import express from "express"
-import mongoose from "mongoose"
-import listEndpoints from "express-list-endpoints"
-import cors from "cors"
+import express from "express";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+import cors from "cors";
+import authorsRouter from "./services/authors/index.js";
+import blogsRouter from "./services/blogs/index.js";
+import commentsRouter from "./services/comments/index.js";
+import {
+  badRequestHandler,
+  notFoundHandler,
+  genericErrorHandler,
+} from "./errorHandlers.js";
 
-import blogsRouter from "./services/blogs/index.js"
-import commentsRouter from "./services/comments/index.js"
-import { badRequestHandler, notFoundHandler, genericErrorHandler } from "./errorHandlers.js"
-
-const server = express()
-const port = process.env.PORT || 3001
+const server = express();
+const port = process.env.PORT || 3001;
 
 // ***************************************** MIDDLEWARES **************************************
 
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
 // ****************************************** ENDPOINTS ***************************************
 
-server.use("/blogs", [blogsRouter, commentsRouter])
-server.use("/blogs", [blogsRouter, commentsRouter])
+server.use("/blogs", [blogsRouter, commentsRouter]);
+server.use("/blogs", [blogsRouter, commentsRouter]);
+server.use("/authors", authorsRouter);
 
 // ***************************************** ERROR HANDLERS ***********************************
 
-server.use(badRequestHandler)
-server.use(notFoundHandler)
-server.use(genericErrorHandler)
+server.use(badRequestHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
-mongoose.connect(process.env.MONGODB_CONNECTION)
+mongoose.connect(process.env.MONGODB_CONNECTION);
 
 mongoose.connection.on("connected", () => {
-    console.log("Successfully connected to Mongo!")
+  console.log("Successfully connected to Mongo!");
 
-    server.listen(port, () => {
-        console.table(listEndpoints(server))
-        console.log(`Server running on port ${port}`)
-    })
-})
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
+    console.log(`Server running on port ${port}`);
+  });
+});
